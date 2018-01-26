852663115d058132855362d131034c4acbbbd7d6
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-01-25 20:35:28 -0500

    Add component to show list of videos

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ const React = require("react"); // both are valid ways to import
 const ReactDOM = require("react-dom"); // used to inject React components to DOM
 //const SearchBar = require("./components/search_bar.js"); // doesn't work! WHY?
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const YTSearch = require("youtube-api-search");
+
 const API_KEY = "AIzaSyBSvoOrN5v_3oYtaJpm9KoNOMeFvOjpjeE";
 
-YTSearch({
-    key: API_KEY,
-    term: 'crazy kittens'
-}, function(data) {
-    console.log(data);
-});
-
 // create component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos : [] };
+        const self = this;
+
+        YTSearch({
+            key: API_KEY,
+            term: 'crazy kittens'
+        }, videos => {
+            this.setState({ videos });
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
