5d179f1cf8eaf6f6ca4b56cad28008f35aec4270
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-02-08 14:58:53 -0500

    some basic styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,11 +14,8 @@ class SearchBar extends React.Component {
     */
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input value = {this.state.term} onChange = { event => this.setState({ term : event.target.value }) } />
-                <div>
-                    Value of the input: {this.state.term}
-                </div>
             </div>
         );
     }
